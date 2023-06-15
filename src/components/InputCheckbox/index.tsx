3af414ee8a95b435135db30059cf02e6ba024eac
“indavid” <david@gmail.com>
2023-06-14 23:53:16 -0400

    commit to main

---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames"
+import { useRef } from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+  console.log("disable", disabled)
+  return (
+    <div className="RampInputCheckbox--container" data-testid={inputId}>
+      <label
+        className={classNames("RampInputCheckbox--label", {
+          "RampInputCheckbox--label-checked": checked,
+          "RampInputCheckbox--label-disabled": disabled,
+        })}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className={classNames("RampInputCheckbox--input", {
+          "RampInputCheckbox--input-checked": checked,
+          "RampInputCheckbox--input-disabled": disabled,
+        })}
+        checked={checked}
+        disabled={disabled}
+        onChange={() => {
+          onChange(!checked)
+          console.log("i ran onChange")
+        }}
+      />
+    </div>
+  )
+}
---
+++ src/hooks/types.ts
@@ -0,0 +1,21 @@
+import { Employee, Transaction } from "../utils/types"
+
+type UseTypeBaseResult<TValue> = {
+  data: TValue
+  loading: boolean
+  invalidateData: () => void
+}
+
+type UseTypeBaseAllResult<TValue> = UseTypeBaseResult<TValue> & {
+  fetchAll: () => Promise<void>
+}
+
+type UseTypeBaseByIdResult<TValue> = UseTypeBaseResult<TValue> & {
+  fetchById: (id: string) => Promise<void>
+}
+
+export type EmployeeResult = UseTypeBaseAllResult<Employee[] | null>
+
+export type PaginatedTransactionsResult = any
+
+export type TransactionsByEmployeeResult = UseTypeBaseByIdResult<Transaction[] | null>
